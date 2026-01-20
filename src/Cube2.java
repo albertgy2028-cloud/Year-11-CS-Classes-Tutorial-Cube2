d017eb3487875033d525545e5a83dfe53cbddd11
albertgy2028-cloud <albertgy2028@student.cis.edu.hk>
2026-01-20 23:26:33 +0800

    Update Cube2.java

--- src/Cube2.java
+++ src/Cube2.java
@@ -1,7 +1,6 @@
 public class Cube2 {
-    private Cube basicCube;
+    private final Cube basicCube;
     private String color;
-
     public Cube2() {
         basicCube = new Cube();
         this.color = "black";
@@ -12,18 +11,53 @@ public class Cube2 {
         basicCube.setSide(side);
     }
 
+    public Cube2(int side, String color) {
+        this(side);
+        this.color = color;
+    }
+
     public int getSide() {
         return basicCube.getSide();
     }
 
-    //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
+    public void setSide (int Side) {
+        basicCube.setSide(Side);
+    }
 
+    public int calculateVolume() {
+        return basicCube.calculateVolume();
+    }
+
+    public int calculateSurfaceArea() {
+        return basicCube.calculateSurfaceArea();
+    }
+
+    public Cube2 add(Cube2 otherCube) {
+        int A = basicCube.calculateSurfaceArea();
+        int B = otherCube.calculateSurfaceArea();
+        int C = A + B;
+        double sideLength = Math.sqrt(C / 6d);
+        int sideLengthI = (int) sideLength;
+        final double epsilon = 0.001d;
+        if (Math.abs(sideLength - sideLengthI) > epsilon) throw new IllegalArgumentException("Cubes do not form an pythagorean triple");
+            return new Cube2(sideLengthI, color);
+
+    }
+    public Cube2 minus(Cube2 otherCube) {
+        int surf = this.calculateSurfaceArea();
+        int otherSurf = otherCube.calculateSurfaceArea();
+        if (surf - otherSurf <= 0) throw new IllegalArgumentException("Cube surface area - Other cube surface area is less or equal to zero");
+        int goalSurf = surf - otherSurf;
+        double sideLength = Math.sqrt(goalSurf / 6d);
+        int sideLengthI = (int) sideLength;
+        final double epsilon = 0.001d;
+        if (Math.abs(sideLength - (double) sideLengthI) > epsilon) throw new IllegalArgumentException("Cubes do not form an pythagorean triple");
+        return new Cube2(sideLengthI, color);
+    }
+    public boolean equals(Cube2 otherCube) {
+        return (otherCube.basicCube.getSide() == basicCube.getSide() && otherCube.color.equals(this.color));
+    }
     public String toString() {
-        return basicCube.toString();
+        return "Cube{side=" + getSide() + ", color=\"" + color + "\"}";
     }
 }
